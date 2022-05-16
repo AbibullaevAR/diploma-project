e7431895d5f009723c6443ae20810229016980d8
mGfPCEwG <toprup@mail.ru>
2022-05-16 17:17:58 +0300

    fix: url in rests psw

--- accounts/mail_util.py
+++ accounts/mail_util.py
@@ -5,7 +5,7 @@ def send_complete_reg_mail(scheme, host, email):
     send_mail(
         'Регистрация аккаунта diploma project',
         'Вы зарегистрировали аккаунт в diploma project для завершения регистрации перейдите по ссылке и сбросте ' +
-        f'пароль используйте ваш email в качестве username. {scheme}//{host}/' +
+        f'пароль используйте ваш email в качестве username. {scheme}://{host}/accounts/' +
         'rest_password_start/',
         'diploma project@',
         [email, ],
