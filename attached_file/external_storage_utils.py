67da94f118ce619ee5291fae3f69abd846438daa
mGfPCEwG <toprup@mail.ru>
2022-06-07 21:24:34 +0300

    create: ExternalStorageManage add method get_download_link

--- attached_file/external_storage_utils.py
+++ attached_file/external_storage_utils.py
@@ -2,13 +2,29 @@ from django.conf import settings
 import requests
 
 
-def get_upload_link(file_id):
+class ExternalStorageManage:
     headers = {
         'Accept': 'application/json',
         'Authorization': f'OAuth {settings.EXTERNAL_STORAGE_TOKEN}'
     }
-    request_json = requests.get(
-        url=f'https://cloud-api.yandex.net/v1/disk/resources/upload?path={file_id}',
-        headers=headers
-    ).json()
-    return request_json['href']
+
+    action = {
+        'upload_file': 'https://cloud-api.yandex.net/v1/disk/resources/upload?path=',
+        'download_file': 'https://cloud-api.yandex.net/v1/disk/resources/download?path='
+    }
+
+    def get_upload_link(self, file_name: str):
+
+        request_json = requests.get(
+            url=self.action['upload_file'] + file_name,
+            headers=self.headers
+        ).json()
+        return request_json['href']
+
+    def get_download_link(self, file_name):
+
+        request_json = requests.get(
+            url=self.action['download_file'] + file_name,
+            headers=self.headers
+        ).json()
+        return request_json['href']
