6ed786e574a3ddba39b715c772a741bca0b82e20
mGfPCEwG <toprup@mail.ru>
2022-06-07 21:25:57 +0300

    fix: change get_upload_link to ExternalStorageManage.get_upload_link

--- attached_file/api/views.py
+++ attached_file/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework import permissions
 
 from .serializers import CreateUserFileSerializer
-from attached_file.external_storage_utils import get_upload_link
+from attached_file.external_storage_utils import ExternalStorageManage
 
 
 class CreateUserFileView(generics.CreateAPIView):
@@ -15,6 +15,9 @@ class CreateUserFileView(generics.CreateAPIView):
 
     def get_success_headers(self, data):
         headers = super().get_success_headers(data)
-        headers['Upload_URL'] = get_upload_link(str(self.created_instance.pk) + self.created_instance.file_name)
+        ext_manage = ExternalStorageManage()
+        headers['Upload_URL'] = ext_manage.get_upload_link(
+            str(self.created_instance.pk) + self.created_instance.file_name
+        )
         return headers
 
