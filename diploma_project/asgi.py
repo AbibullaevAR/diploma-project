43210570e708637bd36eb750978ef480c88a18dc
Abibullaev <a.alim2019@mail.ru>
2022-07-12 22:28:56 +0300

    fix: asgi import

--- diploma_project/asgi.py
+++ diploma_project/asgi.py
@@ -13,12 +13,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from discussions.websocket import routing
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diploma_project.settings')
 
+django_asgi_app = get_asgi_application()
+
+from discussions.websocket import routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
 
     "websocket": AuthMiddlewareStack(
         URLRouter(
